50fb92e34b518432d6d7329645341df7ee699a40
victor mgbeahurike <victormgbeahurike82@gmail.com>
2024-03-18 03:59:50 +0100

    my first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,13 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1310px",
     },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
